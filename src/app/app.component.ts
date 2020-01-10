bb8a01ef0d3e09f53359b2d1f7b7b8de708cd4c8
诗和远方 <1501725626@qq.com>
2020-01-11 01:19:40 +0800

    ^ 暂时算是把bitmap生成出来了

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,126 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef, HostListener } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { saveData2Json } from './shared/utils';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'printf-bitmap';
+export class AppComponent implements OnInit {
+
+  dotBit: number = 8;
+  dot = 1;
+
+  dotZoom: string = '1x';
+
+  formValue = {};
+
+  form = new FormGroup({
+    dot: new FormControl(),
+    width: new FormControl(),
+    height: new FormControl(),
+    fileName: new FormControl()
+  })
+
+  bitmap: Array<Array<Number>> = [];
+
+  isDownShift: boolean = false;
+
+  pointXY: Array<number[]> = []; // 用于记录用户点击的
+
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(event) {
+    if (event.shiftKey) {
+      console.log('keyDown', event.shiftKey);
+      this.isDownShift = true;
+    }
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  onKeyUp(event) {
+    if (!event.shiftKey) {
+      console.log('keyup', event.shiftKey);
+      this.isDownShift = false;
+    }
+  }
+
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {
+
+  }
+
+
+  ngOnInit() {
+    this.form.setValue({
+      dot: 1,
+      width: 240,
+      height: 300,
+      fileName: 'bitmap'
+    });
+    this.changeBitMap();
+  }
+
+
+  changeBitMap() {
+    const values = this.form.value;
+    this.dot = values.dot;
+
+    if (this.formValue['width'] !== values.width || this.formValue['height'] !== values.height) {
+      this.bitmap = Array.from({ length: values.height }, (cur, idx) => {
+        return Array.from({ length: values.width }, (_, x) => 0);
+      });
+    }
+    this.formValue = { ...values };
+    this.cdr.detectChanges();
+  }
+
+
+  checke(row, dot) {
+
+    this.pointXY.push([row, dot]);
+    const copyBitmap = this.bitmap.slice();
+    // 此处做动作
+    if (this.isDownShift) {
+
+      const [before, current] = this.pointXY;
+      const yn = Math.abs(before[0] - current[0]) + 1;
+      const xn = Math.abs(before[1] - current[1]) + 1;
+
+      const startX = current[0] > before[0] ? before[0] : current[0];
+      const startY = current[1] > before[1] ? before[1] : current[1];
+      console.log('2次坐标', this.pointXY);
+      console.log(`
+        x 移动 ${xn}
+        y 移动 ${yn}
+        startX ${startX}
+        startY ${startY}
+      `);
+
+      copyBitmap[before[0]][before[1]] = 0;
+      for (let i = 0; i < yn; i++) {
+        for (let j = 0; j < xn; j++) {
+          copyBitmap[i + startX][j + startY] = copyBitmap[i + startX][j + startY] ? 0 : 1;
+        }
+      }
+    } else {
+      copyBitmap[row][dot] = copyBitmap[row][dot] ? 0 : 1;
+    }
+    if (this.pointXY.length === 2) {
+      this.pointXY = [];
+    }
+
+    this.bitmap = [...copyBitmap];
+    this.cdr.detectChanges();
+
+  }
+
+
+  download() {
+    const { fileName, width, height } = this.form.value;
+    saveData2Json(this.bitmap, `${width}-${height}-${fileName}.json`);
+  }
 }
---
+++ src/app/shared/utils.ts
@@ -0,0 +1,29 @@
+/**
+* 把json数据保存为文件
+*
+* @param {*} data - json对象
+* @param {string} filename - 文件名
+* @returns
+* @memberof Globals
+*/
+export function saveData2Json(data, filename: string = 'Default.json') {
+
+    if (!data) {
+        console.error('Console.save: No data')
+        return;
+    }
+
+    if (typeof data === "object") {
+        data = JSON.stringify(data, undefined, 4)
+    }
+
+    var blob = new Blob([data], { type: 'text/json' }),
+        e = document.createEvent('MouseEvents'),
+        a = document.createElement('a')
+
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    a.dispatchEvent(e)
+}
